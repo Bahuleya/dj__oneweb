5058f4ad755b3022dc05e437649d86467e471912
Bahuleya <95250227+Bahuleya@users.noreply.github.com>
2021-12-02 01:37:48 +0530

    Update settings.py

--- oneweb/oneweb/settings.py
+++ oneweb/oneweb/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%z1%14%(bbd64o12!xiq6p*ez&p(o-mx=uebn0r+ly1qc!bq*y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1:8000',]
+ALLOWED_HOSTS = ['127.0.0.1:8000','djoneweb.herokuapp.com']
 
 
 # Application definition
